ef968cf49fac8375494e286d079b627047bcd604
woozxn <squib0612@gmail.com>
2022-07-14 16:54:55 +0900

    Initial commit

---
+++ src/test/java/woozxn/springcore/SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package woozxn.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
