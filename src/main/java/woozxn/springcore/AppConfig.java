8ea404564a6fa4d2374c889ac3bc94113f073c3d
woozxn <squib0612@gmail.com>
2022-07-25 23:05:21 +0900

    refactor(woozxn) : Appconfig 리팩터링

--- src/main/java/woozxn/springcore/AppConfig.java
+++ src/main/java/woozxn/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package woozxn.springcore;
 
+import woozxn.springcore.discount.DiscountPolicy;
 import woozxn.springcore.discount.FixDiscountPolicy;
+import woozxn.springcore.member.MemberRepository;
 import woozxn.springcore.member.MemberService;
 import woozxn.springcore.member.MemberServiceImpl;
 import woozxn.springcore.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import woozxn.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
