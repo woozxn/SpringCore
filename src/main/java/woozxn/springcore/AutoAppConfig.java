51789663111061f3dbab56a5a459283d31dca188
woozxn <squib0612@gmail.com>
2022-08-08 23:23:23 +0900

    feat(woozxn) : 중복 등록과 충돌
    수동 등록이 우선이다.

--- src/main/java/woozxn/springcore/AutoAppConfig.java
+++ src/main/java/woozxn/springcore/AutoAppConfig.java
@@ -1,12 +1,20 @@
 package woozxn.springcore;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import woozxn.springcore.member.MemberRepository;
+import woozxn.springcore.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
