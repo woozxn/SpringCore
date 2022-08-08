9015d931ce7586ec373579e6544c48b20814a891
woozxn <squib0612@gmail.com>
2022-08-08 23:17:19 +0900

    feat(woozxn) : 필터

---
+++ src/test/java/woozxn/springcore/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,10 @@
+package woozxn.springcore.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
